9eb78f85c248e1e026420f8de5b851b2a7bff050
samiUllah1526 <sami.naazar1526@gmail.com>
2022-10-22 05:08:24 +0500

    Add empty dependency array in useMobile hook

--- src/hook.js
+++ src/hook.js
@@ -5,11 +5,11 @@ export const useMobile = (givenWidth = 375) => {
     const [width, setWidth] = useState(window.outerWidth)
 
     const handleResize = () => setWidth(window.outerWidth)
-    
+
     useEffect(() => {
         window.addEventListener('resize', handleResize)
         return _ => window.removeEventListener('resize', handleResize)
-    })
+    }, [])
 
     return width <= givenWidth;
 
